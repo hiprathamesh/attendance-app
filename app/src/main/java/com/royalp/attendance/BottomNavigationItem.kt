853e651ac42b46852a993e69a2b5cdfc44aeaf3b
hiprathamesh <prathameshnikam21119@gmail.com>
2024-08-28 18:13:10 +0530

    add navigation bar

---
+++ app/src/main/java/com/royalp/attendance/BottomNavigationItem.kt
@@ -0,0 +1,9 @@
+package com.royalp.attendance
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavigationItem (
+    var title: String,
+    var selected: ImageVector,
+    var unSelected: ImageVector,
+)
--- app/src/main/java/com/royalp/attendance/MainActivity.kt
+++ app/src/main/java/com/royalp/attendance/MainActivity.kt
@@ -6,9 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.royalp.attendance.ui.theme.AttendanceTheme
@@ -19,29 +30,60 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AttendanceTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val items = listOf(
+                    BottomNavigationItem(
+                        title = "Home",
+                        selected = Icons.Filled.Home,
+                        unSelected = Icons.Outlined.Home,
+                    ),
+                    BottomNavigationItem(
+                        title = "Profile",
+                        selected = Icons.Filled.Home,
+                        unSelected = Icons.Outlined.Home,
+                    ),
+                    BottomNavigationItem(
+                        title = "Settings",
+                        selected = Icons.Filled.Home,
+                        unSelected = Icons.Outlined.Home,
+                    )
+                )
+                var selectedItemIndex by rememberSaveable {
+                    mutableIntStateOf(0)
+                }
+                Scaffold (
+                    bottomBar = {
+                        NavigationBar {
+                            items.forEachIndexed { index, item ->
+                                NavigationBarItem(
+                                    selected = selectedItemIndex == index,
+                                    onClick = {
+                                        selectedItemIndex = index
+                                        //navController.navigate(item.title)
+                                    },
+                                    label = { Text(text = item.title) },
+                                    icon = {
+                                        BadgedBox(
+                                            badge = {}
+                                        ) {
+                                            Icon(
+                                                imageVector = if (index == selectedItemIndex) {
+                                                    item.selected
+                                                } else item.unSelected,
+                                                contentDescription = item.title
+                                            )
+                                        }
+                                    }
+                                )
+                            }
+                        }
+                    }
+                ){
+                    Text(
+                        text = "Hello World!",
+                        modifier = Modifier.padding(it)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AttendanceTheme {
-        Greeting("Android")
     }
 }
